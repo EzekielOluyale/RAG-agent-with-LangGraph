1b3faa4e7cb10d6300617e006833b33fa020d747
Oluyale Ezekiel <amusecode@gmail.com>
2026-06-07 22:15:37 +0100

    updated code

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,4 @@
 from dotenv import load_dotenv
 
-from langchain_community.document_loaders import (
-    DirectoryLoader,
-    PyPDFLoader
-)
-
 def setup_environment():
     load_dotenv()
-
-def load_documents():
-    loader = DirectoryLoader(
-        "../data",
-        glob="*.pdf",
-        loader_cls=PyPDFLoader
-    )
-
-    return loader.load()
